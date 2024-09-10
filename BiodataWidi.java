8094976eeb35e58f365333ffd73eda6ee3df307e
NewUserWIDI <wwidi4087@gmail.com>
2024-09-10 14:02:30 +0700

    Biodata widi widayanti

---
+++ BiodataWidi.java
@@ -0,0 +1,12 @@
+public class BiodataWidi {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nama         : Widi Widayanti");
+        System.out.println("TTL          : Malang, 16 Feb 2006");
+        System.out.println("Jurusan      :Teknologi Informasi");
+        System.out.println("Prodi        :D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat       :Jl.Dharma Bakti, RT22 RW03");
+        System.out.println("No.HP        :082142127532");
+    }
+}
