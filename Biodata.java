a23498eb903143600f931b4bf0b4583821100506
shintadella <vaizatusdella@gmail.com>
2024-09-10 13:40:05 +0700

    Biodata Shinta Della W

---
+++ Biodata.java
@@ -0,0 +1,16 @@
+public class Biodata {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Nama        : Shinta Della Widyanti");
+        System.out.println("NIM         : 244107060157");
+        System.out.println("Kelas       : SIB 1B");
+        System.out.println("No Absen    : 22");
+        System.out.println("Prodi       : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat      : Jl.Senggani No.3");
+        System.out.println("No HP       : 082144520659");
+    }
+
+}
