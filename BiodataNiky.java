16fa3a1c4a3c9d8aa2edd38b0eb406445d5057c7
nikytania <nikytania0701@gmail.com>
2024-09-10 14:16:36 +0700

    Biodata Niky Tania Sari

---
+++ BiodataNiky.java
@@ -0,0 +1,15 @@
+public class BiodataNiky {
+    public static void main(String[] args){
+        System.out.println("--------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi Polinema--");
+        System.out.println("Nama     : Niky Tania Sari");
+        System.out.println("NIM      : 244107060053");
+        System.out.println("Kelas    : 1B");
+        System.out.println("No Absen : 18");
+        System.out.println("Prodi    : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat   : Jl. Simpang Remujung");
+        System.out.println("No HP    : 085604892110");
+        System.out.println("Hobi     : Menjelajahi Alam");
+
+    }
+}
